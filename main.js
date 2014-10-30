359346e027a45f7524878778ec30f043f931e100
Merlijn van der Kamp <zemanzo.my@gmail.com>
2014-10-30 21:48:09 +0100

    Many small fixes. Rewrote parts for O.o(), parts will now move automatically when changing board.pieces. Added a ton of (colored!) console.logs to search for a bug thats preventing proper paths again.

--- main.js
+++ main.js
@@ -52,6 +52,7 @@ function init(){
 	board.pieces = {};	
 	
 	// syntax: startSetup(startingRow-1,endingRow,type);
+	// This is default set to size 10, in case size changes, make sure to either change up this to do it automatically or at least do it manually!
 	startSetup(0,4,"black");
 	startSetup(4,6,"null");
 	startSetup(6,board.size,"white");
@@ -67,7 +68,7 @@ function init(){
 	}
 
 	// Indicate starting player
-	document.getElementById("player").innerHTML = board.currentPlayer;
+	//document.getElementById("player").innerHTML = board.currentPlayer;
 }
 
 function startSetup(rowStart,rowEnd,color){
@@ -107,6 +108,7 @@ function startSetup(rowStart,rowEnd,color){
 				if (board.turns == 0){	// On starting, change the header to display info
 					document.getElementById("header").style.fontSize = "20px";
 					document.getElementById("header").innerHTML = '<div class="headerInfo" style="width:30%">Current player: <div id="player">White</div></div><div class="headerInfo" style="width:30%; font-size:.9em;">Turn: <span id="turns">0</span><br/>Time playing: <span id="time">00:00</span></div><div class="headerInfo" style="width:40%;">Info per player here</div>';
+					// Simple timer (per second)
 					var seconds = 0;
 					var minutes = 0;
 					var zMin,zSec;
@@ -139,7 +141,7 @@ function startSetup(rowStart,rowEnd,color){
 				} else if (selected == selectedId) {															// Same pieces is selected, thus a deselect is performed
 					selected = "null";
 					selectedPiece.style.border = "none";
-					clearMoveset()
+					clearMoveset();
 				} else if (type == board.currentPlayer) {														// Other piece is selected, previous will be deselected and new one will be selected
 					document.getElementById(selected).style.border = "none";
 					selected = selectedId;
@@ -148,11 +150,11 @@ function startSetup(rowStart,rowEnd,color){
 				}
 			};
 			board.pieces[rows][i].getMoveset = function(){	// Function to get moveset
-				console.log("%c Getting new moveset ","border-left:rgb(255,0,0) 3px solid; background-color:rgba(255,0,0,.5);");
-				clearMoveset();
+				clearMoveset(this);
 				var x = this.position.x;
 				var y = this.position.y;
 				var type = this.type;
+				console.log("%c Getting new moveset for "+x+","+y+" ("+type+") ","border-left:rgb(255,0,0) 3px solid; background-color:rgba(255,0,0,.5);");
 				var caller = this;
 				var possibleHits = [];
 				var nextMove = [];
@@ -170,12 +172,13 @@ function startSetup(rowStart,rowEnd,color){
 				}
 				
 				function checkAround(p,fp){
-					//console.log("%c ITERATION "+hitIteration,"border-left:rgb(90,90,255) 3px solid; background-color:rgba(90,90,255,.5);");
+					console.log("%c CHECKAROUND ","border-left:rgb(90,90,255) 3px solid; background-color:rgba(90,90,255,.5);");
 					//console.log("%c PATH "+p,"border-left:rgb(90,255,90) 3px solid; background-color:rgba(90,255,90,.5);");
 					if (hitIteration > 0){			// On any iteration other than the first, look from another cell than the initial one!
-						console.log(nextMove)
+						console.log("Next move is: ",nextMove[p]);
 						x = nextMove[p].x;
 						y = nextMove[p].y;
+						console.log(x,y);
 						caller.landingCells[p].push(x+"_"+y);
 					}
 					colorCell(x,y,"#f90","debug");
@@ -203,6 +206,7 @@ function startSetup(rowStart,rowEnd,color){
 											possibleHits.push((x+a)+"_"+(y+b));						// Add it to the array before treating it
 											tempNext.push(x+a*2);		
 											tempNext.push(y+b*2);
+											console.log("(path ",p,") tempNext = ",tempNext);
 										}
 										//console.log("Check if cell is free to land on: ",board.pieces[x+a*2][y+b*2]);
 										//console.log("Checking for path: ",p," and for iteration: ",hitIteration);
@@ -220,42 +224,57 @@ function startSetup(rowStart,rowEnd,color){
 							}
 						}
 					}
-					function nextCheckAround(fp){
+					// Note that this is a separate function and is NOT called immediately!
+					function nextCheckAround(fp){ // fp is used to check if its the final path in the loop we're checking until moving on to the next big cycle, where p is the current path we're checking
 						//console.log(p,fp);
 						if (p == fp){ // If this is the last path we're checking, add 1 to the hitIteration
 							hitIteration++;
 						}
 						if (possibleHits.length > 0){
-							//console.log("Current paths: ",caller.currentPaths);
 							for (p = 0; p < caller.currentPaths.length; p++){	// For every current path
-								//console.log(tempNext);
+								console.log("tempNext before nextMove (",p,") = ",tempNext);
 								nextMove[p] = {									// Another array for the position of the next iteration
-									x:tempNext[0],
-									y:tempNext[1]
+									x:tempNext[2*p],
+									y:tempNext[2*p+1]
 								};
+								console.log("nextMove[p] = ",nextMove[p]);
 								if (!caller.landingCells[p]){
 									caller.landingCells.push([]);
 								}
-								tempNext = [];
-								possibleHits = [];
 								var fp = caller.currentPaths.length-1;
+							}
+							
+							tempNext = [];
+							possibleHits = [];
+							
+							for (p = 0; p < caller.currentPaths.length; p++){
 								checkAround(p,fp);
 							}
 						} else {
 							console.log("No new hits are found");
-							// Done processing, color all hits dark red for debug purposes.
+							// Done processing
+							// Add an eventlistener to all possible landing cells
+							for (a = 0; a < caller.landingCells.length; a++){
+								for (b = 0; b < caller.landingCells[a].length; b++){
+									if (b == caller.landingCells[a].length-1){ // Make sure only the end of the paths are clickable
+										document.getElementById(caller.landingCells[a][b]).style.cursor = "pointer";
+										document.getElementById(caller.landingCells[a][b]).dataset.path = a;
+										document.getElementById(caller.landingCells[a][b]).addEventListener("click", movePiece, false);
+										console.log("%c Landing cell created on path "+a+" found at "+caller.currentPaths[a][b].split("_")[0]+","+caller.currentPaths[a][b].split("_")[1],"border-left:rgb(255,128,0) 3px solid;");
+									}
+								}
+							}
+							// Color all hits dark red for debug purposes
 							for (a = 0; a < caller.currentPaths.length; a++){
 								for (b = 0; b < caller.currentPaths[a].length; b++){
 									colorCell(caller.currentPaths[a][b].split("_")[0],caller.currentPaths[a][b].split("_")[1],"#b00","debug");
-									if (b == caller.currentPaths[a].length-1){
-										document.getElementById(caller.currentPaths[a][b]).style.cursor = "pointer";
-										document.getElementById(caller.currentPaths[a][b]).dataset.path = a;
-										document.getElementById(caller.currentPaths[a][b]).addEventListener("click", movePiece, false);
-									}
+									console.log("%c Hit on path "+a+" found at "+caller.currentPaths[a][b].split("_")[0]+","+caller.currentPaths[a][b].split("_")[1],"border-left:rgb(128,0,0) 3px solid;");
 								}
 							}
 						}
 					}
+					
+					// Code continues processing here
 					if (hitIteration > 0){								// If more hits are found after the first one
 						for (i = 0; i < possibleHits.length; i++){		// Treat hits
 							if (i > 0){									// If there's more than one hit, a new path is found
@@ -270,7 +289,7 @@ function startSetup(rowStart,rowEnd,color){
 							console.log(caller.currentPaths);
 						}
 						nextCheckAround(fp);
-					} else {											// Only for the first hit
+					} else {											// ONLY ON THE FIRST HIT
 						for (i = 0; i < possibleHits.length; i++){		// Treat hits
 							caller.currentPaths.push([]);
 							caller.currentPaths[i][hitIteration] = possibleHits[i];	// On the first iteration, create a new path for every hit
@@ -292,15 +311,27 @@ function startSetup(rowStart,rowEnd,color){
 			Object.observe(board.pieces[rows][i],function(changes){
 				changes.forEach(function(change) {
 					if (change.name == "type"){
+						console.log(change);
 						var x = change.object.position.x;
 						var y = change.object.position.y;
 						var element = document.getElementById("piece-"+x+"_"+y);
-						if (change.object.type == "null"){
+						if (change.object.type == "null"){ // If the piece type is changed to null, its removed.
 							element.parentElement.removeChild(element);
-						} else if (change.object.type == "white"){
-							
-						} else if (change.object.type == "black"){
-							
+							document.getElementById(element).removeEventListener('click',function(){
+								board.pieces[this.id.substring(0,1)][this.id.substring(2)].selectPiece();
+							},false); // UNTESTED!!!
+						} else if (change.object.type == "white"|| change.object.type == "black"){
+							console.log(element);
+							document.getElementById(x+"_"+y).innerHTML += '<div data-type="'+change.object.type+'" id="piece-'+x+'_'+y+'" class="'+change.object.type+'Piece checkersPiece">&nbsp;</div>';
+							document.getElementById(x+"_"+y).addEventListener('click',function(){
+								board.pieces[this.id.substring(0,1)][this.id.substring(2)].selectPiece();
+							},false);
+							if (y == 0 || y == 9){
+								movedPiece.style.backgroundImage = "url('crown.png')";
+								element.dataset.type = col+"King";
+							} else {
+								element.dataset.type = col;
+							}
 						}
 					}
 				});
@@ -309,8 +340,12 @@ function startSetup(rowStart,rowEnd,color){
 	}
 }
 
-function clearMoveset(){
+function clearMoveset(obj){
 	// Clear previous preview
+	if (obj){
+		obj.currentPaths = [];
+		obj.landingCells = [];
+	}
 	hitAmount = 0;
 	for (i = 0; i < coloredCells.length; i++){
 		document.getElementById(coloredCells[i]).style.backgroundColor = "#630";
@@ -330,12 +365,14 @@ function colorCell(x,y,color,type){
 		document.getElementById(x+"_"+y).style.backgroundColor = "#99f";
 		document.getElementById(x+"_"+y).style.cursor = "pointer";
 		document.getElementById(x+"_"+y).addEventListener("click", movePiece, false);
+		console.log("%c Simple hit found and created at "+x+","+y+" ("+type+")","border-left:rgb(0,0,255) 3px solid;");
 	} else if (type == "debug"){
 		document.getElementById(x+"_"+y).style.backgroundColor = color;
 	}
 	coloredCells.push(x+"_"+y);
 }
 
+// This function is called when the player clicks an empty spot to move its checkers piece to.
 function movePiece(){
 	var w = "white";
 	var b = "black";
@@ -347,8 +384,11 @@ function movePiece(){
 		board.currentPlayer = w;
 		move(b,this);
 	}
+	// In case a path was found in the data of the moved piece, remove all hit pieces.
 	if (path){
 		var pos = this.id.split("_");
+		console.log(board.pieces[pos[0]][pos[1]]);
+		console.log(path);
 		for (i = 0; i < board.pieces[pos[0]][pos[1]].currentPaths[path].length; i++){
 			board.pieces[pos[0]][pos[1]].type = "null";
 		}
@@ -361,18 +401,19 @@ function move(col,e){
 		s = selected.split("_");
 		m = e.id.split("_");
 		s[0] = s[0].substring(6);
-		board.pieces[s[0]][s[1]].type = "null";
+		console.log("%c Moving piece ("+s[0]+","+s[1]+") to ("+m[0]+","+m[1]+") ","border-left:rgb(255,128,0) 3px solid; background-color:rgba(255,128,0,.5);");
+		board.pieces[s[0]][s[1]].type = "null"; // These call the Object.observe() we made earlier!
 		board.pieces[m[0]][m[1]].type = col;
 		//document.getElementById(selected).parentElement.removeChild(document.getElementById(selected));
-		document.getElementById(e.id).innerHTML += '<div data-type="'+col+'" id="piece-'+m[0]+'_'+m[1]+'" class="'+col+'Piece checkersPiece">&nbsp;</div>';
-		document.getElementById(e.id).addEventListener('click',function(){board.pieces[this.id.substring(0,1)][this.id.substring(2)].selectPiece()},false);
-		var movedPiece = document.getElementById('piece-'+m[0]+'_'+m[1]);
+		//document.getElementById(e.id).innerHTML += '<div data-type="'+col+'" id="piece-'+m[0]+'_'+m[1]+'" class="'+col+'Piece checkersPiece">&nbsp;</div>';
+		
+		/*var movedPiece = document.getElementById('piece-'+m[0]+'_'+m[1]);
 		if (m[0] == 0 || m[0] == 9){
 			movedPiece.style.backgroundImage = "url('crown.png')";
 			movedPiece.dataset.type = col+"King";
 		} else {
 			movedPiece.dataset.type = col;
-		}
+		}*/
 		// Clean up
 		for (x = 0; x < board.size; x++){
 			for (y = 0; y < board.size; y++){
